f0ac6fb84e8e8a8329b61dc08713e019fff75464
Teresa Lamb <flour1337@gmail.com>
2014-12-08 00:38:03 -0500

    2 sensors

--- fur2/fur2.ino
+++ fur2/fur2.ino
@@ -1,162 +1,141 @@
 int base0 = 0;
 int base1 = 0;
-int base2 = 0;
-int base3 = 0;
 
 int base0total; 
 int base1total;
-int base2total;
-int base3total;
 
 int new0 = 0;
 int new1 = 0;
-int new2 = 0;
-int new3 = 0;
 
-int new0total =0 ; 
+int new0total = 0; 
 int new1total = 0;
-int new2total = 0;
-int new3total = 0;
 
 int diff0 = 0;
 int diff1 = 0;
-int diff2 = 0;
-int diff3 = 0;
 
 boolean firstRead = true;
 boolean leftPressed = false;
 boolean rightPressed = false;
-count++;
-countMax = 10;
-threshold = 20;
+int count = 0;
+int countMax = 10;
 
-//const int vibPin = 3;
-//const int ledPin0 = 5;
-//const int ledPin1 = 6;
+const int vibPin = 3;
+const int ledPin0 = 5;
+const int ledPin1 = 6;
 
-void setup() {
-  // initialize serial communications at 9600 bps:
+void setup(){
   Serial.begin(9600);
-  Serial.println("0,0,0,0");
+  Serial.println("0,0");
 
-  //pinMode(vibPin, OUTPUT);
-  //pinMode(ledPin0, OUTPUT);
-  //pinMode(ledPin1, OUTPUT);
+  pinMode(vibPin, OUTPUT);
+  pinMode(ledPin0, OUTPUT);
+  pinMode(ledPin1, OUTPUT);
 
 }
 
 void loop() {
-  readValues();
-  checkValues();
-  reset();
 
-  //  digitalWrite(ledPin0, LOW);
-  //  digitalWrite(ledPin1, LOW);
-}
-
-readValues() {
   int analogValue0 = analogRead(A0);
   int analogValue1 = analogRead(A1);
-  int analogValue2 = analogRead(A2);
-  int analogValue3 = analogRead(A3);
 
   count++;
 
-  if(firstRead) { // first run
+  if(firstRead) {
+    digitalWrite(vibPin, LOW);
+    digitalWrite(ledPin0, LOW);
+    digitalWrite(ledPin1, LOW);
+
+    //total first set of readings
     base0total += analogValue0;
     base1total += analogValue1;
-    base2total += analogValue2;
-    base3total += analogValue3;
 
+    //find base average
     if(count == countMax){
       base0 = base0total/count;
       base1 = base1total/count;
-      base2 = base2total/count;
-      base3 = base3total/count;
 
-      count = 0;
       firstRead = false;
+      count = 0;
     } 
   } 
   else { // all subsequent runs
+    //total new set of readings
     new0total += analogValue0;
     new1total += analogValue1;
-    new2total += analogValue2;
-    new3total += analogValue3;
 
+    //find new average
     if(count == countMax){
       new0 = new0total/count;
       new1 = new1total/count;
-      new2 = new2total/count;
-      new3 = new3total/count;
+
+      diff0 = new0 - base0;
+      diff1 = new1 - base1;
+
+      //Serial.println(diff0);
+      if (leftPressed){
+        digitalWrite(vibPin, HIGH);
+        
+        if(rightPressed){
+          Serial.println("both pressed");
+        } 
+        else if (diff0 > 12){
+          //left is still pressed
+        } 
+        else if (diff1 > 7){
+          //direction is correct
+          Serial.println("moving right");
+        } 
+        else {
+          //neither is pressed
+          leftPressed = false;
+          rightPressed = false;       
+        } 
+
+        if (diff0 > 12){
+          leftPressed = true;
+        }
+        if (diff1 > 7){
+          rightPressed = true;
+        }
+
+      }
+
+      //      if (diff0 <= 12){
+      //        leftPressed = false;
+      //      }
+      //      if (diff1 <= 7){
+      //        rightPressed = false;
+      //      } 
+
+
+      delay(50);
+
+      base0 = new0;
+      base1 = new1;
+
+      new0total = 0;
+      new1total = 0;
+
+      count = 0;
     }
   }
-}
-
-checkValues() {
-  diff0 = new0 - base0;
-  diff1 = new1 - base1;
-  diff2 = new2 - base2;
-  diff3 = new3 - base3;
-
   if (leftPressed) {
-    if (rightPressed){
-      // both are pressed
-    } 
-    else { // if only left is pressed, check for movement  
-      if (diff0 > threshold || diff1 > threshold){
-        // left is still pressed
-      } 
-      else if (diff2 > threshold || diff3 > threshold) {
-        //direction is correct
-        leftPressed = false;
-        rightPressed = true;
-      }
-      else {
-        // neither is pressed
-        leftPressed = false;
-        rightPressed = false; 
-      }
-    } 
-  } 
-  else if (rightPressed){
-    if (diff2 > threshold || diff3 > threshold){
-      // right is still pressed 
-    } 
-    else if (diff0 > threshold || diff1 > threshold) {
-      //direction is wrong
-      leftPressed = true;
-      rightPressed = false;
-    } 
-    else {
-      // neither is pressed
-      leftPressed = false;
-      rightPressed = false;
-    }
+    digitalWrite(vibPin, HIGH);
   }
-  else { 
-    if (diff0 > threshold || diff1 > threshold){
-      leftPressed = true;
-    }
-
-    if (diff2 > threshold || diff3 > threshold){
-      rightPressed = true;
-    }
+  if (rightPressed) {
+    digitalWrite(ledPin0, HIGH);
   }
-}
-
-
-reset() {
-  base0 = new0;
-  base1 = new1;
-  base2 = new2;
-  base3 = new3;
-  new0total = 0;
-  new1total = 1;
-  new2total = 2;
-  new3total = 3;
-
-  count = 0;
+  if (leftPressed == false){
+    digitalWrite(vibPin, LOW);
+  }
+  if (rightPressed == false) {
+    digitalWrite(ledPin0, LOW);
+  }
+  //  if(leftPressed == false && rightPressed == false){
+  //    digitalWrite(vibPin, LOW);
+  //    digitalWrite(ledPin0, LOW);
+  //    digitalWrite(ledPin1, LOW);
+  //  }
 }
 
 
